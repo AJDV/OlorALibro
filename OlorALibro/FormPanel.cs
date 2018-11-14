6225ea4262087c7ad63f822c80fd293017936807
cep <cep>
2018-11-14 13:08:03 +0100

    Revert "Merge branch 'Victor'"
    This reverts commit 369bda4cbe2d18cde67c87a12a3a384556e49c11, reversing
    changes made to dca9390449000957e510d9adc8e28fb0b1e718b9.

--- OlorALibro/FormPanel.cs
+++ OlorALibro/FormPanel.cs
@@ -1,4 +1,3 @@
-/*
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -37,34 +36,3 @@ namespace OlorALibro.CRUD_users
         }
     }
 }
-*/
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace OlorALibro.CRUD_users
-{
-    public partial class formPanel : Form
-    {
-        string usuario;
-
-        public formPanel(string usuario)
-        {
-            InitializeComponent();
-            this.usuario = usuario;
-        }
-
-        private void buttonLibreria_Click(object sender, EventArgs e)
-        {
-            FormLibrerias f = new FormLibrerias();
-            f.ShowDialog();
-        }
-    }
-}
-
